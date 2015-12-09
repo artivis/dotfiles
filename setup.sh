65efbd1ca4b416d1084160fad2abd8a4379687cc
Jeremie Deray <deray.jeremie@gmail.com>
2015-12-09 23:23:13 +0100

    working on more choice if file exist

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ TARGET_BASH_ALIASES=$HOME/.bash_aliases
 TARGET_TERMINATOR=$HOME/.config/terminator/config
 
 TARGET_FILES=($TARGET_VIM $TARGET_GIT_CONFIG $TARGET_BASH_ALIASES $TARGET_TERMINATOR)
+#TARGET_FILES=($HOME/toto)
 
 SOURCE_BASE=$(pwd)
 
@@ -23,33 +24,55 @@ SOURCE_BASH_ALIASES=$SOURCE_BASE/.bash_aliases
 SOURCE_TERMINATOR=$SOURCE_BASE/terminator/config
 
 SOURCE_FILES=($SOURCE_VIM $SOURCE_GIT_CONFIG $SOURCE_BASH_ALIASES $SOURCE_TERMINATOR)
+#SOURCE_FILES=($SOURCE_BASE/toto)
 
 for ((i=0;i<${#SOURCE_FILES[@]};++i)); do
 
 	source=${SOURCE_FILES[i]}
 	target=${TARGET_FILES[i]}
 
+	REP=NOTHEREARENOENTRYYET
+
 	if [ -f "$target" ]; then
 
-	  echo -e "\nFile $target exists. Do you want to replace it ? (original file will be moved to $target.old) [Y/n]"
-	  REP=aa
+	  while [ "${REP,,}" != "r" ] && [ "${REP,,}" != "sr" ] && [ "${REP,,}" != "" ]; do
 
-	  while [ "${REP,,}" != "y" ] && [ "${REP,,}" != "n" ] && [ "${REP,,}" != "" ]; do
+		echo -e "\nFile $target exists, what to do [r, s, q] ?"
+
 		read REP
+
+		if [ "${REP,,}" == "q" ]; then
+			exit 0
+		elif [ "${REP,,}" == "r" ]; then
+			echo -e "\nReplacing $target with $source :"
+			echo -e "\t- Moving $target to $target.old"
+			mv $target $target.old
+			echo -e "\t- Copying $source to $target"
+			cp $source $target
+			continue
+		elif [ "${REP,,}" == "s" ] | [ "${REP,,}" == "" ]; then
+			echo -e "\nMoving $target to $target.old:"
+			echo -e "\nReplacing $target with $source :"
+			echo -e "\t- Moving $target to $target.old"
+			mv $target $target.old
+			echo -e "\t- Copying $source to $target"
+			cp $source $target
+			continue
+		else
+			echo "---------------------- Lol wrong ! ---------------------"
+			echo "[r] - replace. Replace whatever is there blindly."
+			echo "[s] - safe. Move old config and copy new one (Default)."
+			echo "[q] - quit."
+			echo "--------------------------------------------------------"
+		fi
+
 	  done
-	  
-	  if [ "${REP,,}" == "y" ] | [ "${REP,,}" == "" ]; then
-		echo -e "\nReplacing $target with $source :"
-		echo -e "\t- Moving $target to $target.old"
-		mv $target $target.old
-		echo -e "\t- Copying $source to $target"
-		cp $source $target
-	  fi
-	  
+
 	else
 		echo -e "\t- Copying $source to $target"
 		cp $source $target
 	fi
+
 done
 
 echo "Done."
