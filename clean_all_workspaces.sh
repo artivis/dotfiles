a98556596c94cff29534ad0dafcb1ed01c56df8f
artivis <deray.jeremie@gmail.com>
2018-12-01 18:50:47 +0100

    cleanup script

--- clean_all_workspaces.sh
+++ clean_all_workspaces.sh
@@ -8,7 +8,7 @@ do
   echo 'Cleaning workspace '${w}
   cd ${w}
 #  echo 'calling: catkin clean --all-profiles -y in '$(pwd)
-#  catkin clean --all-profiles -y
+  catkin clean --all-profiles -y
   cd ..
 done
 
